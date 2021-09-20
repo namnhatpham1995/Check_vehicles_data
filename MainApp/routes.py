29d3159bfb777270b4397c846f4dbfe456a08614
namnhatpham1995 <namnhatpham1995@gmail.com>
2021-09-20 09:22:30 +0200

    Update

--- MainApp/routes.py
+++ MainApp/routes.py
@@ -8,18 +8,49 @@ from urllib.parse import urlparse
 
 id = ""
 secret = ""
+API_URL = 'https://api.mercedes-benz.com/experimental/connectedvehicle/v2/vehicles'
+AUTH_URL = 'https://id.mercedes-benz.com/as/authorization.oauth2'
+
+# Default page
+@app.route('/')
+@app.route('/index')
+def index():
+    code = flask.request.args.get("code")
+    if code is not None:
+        print(code)
+        return redirect(url_for('get_token', code=code))
+    else:
+        return redirect(url_for('login'))
 
 
-# Main route to check json request
+# Page to insert ID and Secret key
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if not request.form['username'] and not request.form['password']:  # Check if the log in form is not filled
+            flash('Please enter username and password!', 'error')
+        else:
+            global id
+            global secret
+            id = request.form['username']  # take user's identity from input
+            secret = request.form['password']
+            print(id)
+            print(secret)
+            return redirect(url_for('get_auth_code'))
+    return render_template('login.html')
+
+
+# Get authentication code used for getting token
 @app.route('/get_auth_code', methods=['GET', 'POST'])
 def get_auth_code():
     print(id)
     host = urlparse(request.base_url)
     redirect_uri = "http://" + host.hostname
-    url = "https://id.mercedes-benz.com/as/authorization.oauth2?response_type=code&redirect_uri=" + redirect_uri + ":5000/&client_id=" + id + "&scope=mb:vehicle:status:general mb:user:pool:reader offline_access "
+    url = AUTH_URL + "?response_type=code&redirect_uri=" + redirect_uri + ":5000/&client_id=" + id + "&scope=mb:vehicle:status:general mb:user:pool:reader offline_access "
     return redirect(url)
 
 
+# Get token
 @app.route('/get_token/<code>', methods=['GET', 'POST'])
 def get_token(code):
     Encoded_ID_Secret = (base64.b64encode((id + ":" + secret).encode("ascii"))).decode("ascii")
@@ -39,32 +70,26 @@ def get_token(code):
     # contentType = response.headers.get("content-type")
     # print(contentType)
     response_json = response.json()
-    print(response_json['access_token'])
+    global access_token
+    global refresh_token
+    access_token = response_json['access_token']
+    refresh_token = response_json['refresh_token']
+    print(response.content)
+    return redirect(url_for('call_api'))
+
+
+# Call API using token
+@app.route('/call_api', methods=['GET', 'POST'])
+def call_api():
+    try:
+        headers = {
+            "authorization": "Bearer " + access_token
+        }
+        print(headers)
+    except:
+        print('No available token')
+
+    response = requests.get(API_URL, headers=headers)
+    response_json = response.json()
+    error_code = response_json['code']
     return response.content
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        if not request.form['username'] and not request.form['password']:  # Check if the log in form is not filled
-            flash('Please enter username and password!', 'error')
-        else:
-            global id
-            global secret
-            id = request.form['username']  # take user's identity from input
-            secret = request.form['password']
-            print(id)
-            print(secret)
-            return redirect(url_for('get_auth_code'))
-    return render_template('login.html')
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    code = flask.request.args.get("code")
-    if code is not None:
-        print(code)
-        return redirect(url_for('get_token', code=code))
-    else:
-        return redirect(url_for('login'))
